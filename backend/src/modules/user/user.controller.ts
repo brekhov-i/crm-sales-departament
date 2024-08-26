142ab3150dd366b48017d2db654874c1103a16f1
brekhov-i <brekhov-i@yandex.ru>
2024-08-26 14:22:44 +0300

    upcommit

--- backend/src/modules/user/user.controller.ts
+++ backend/src/modules/user/user.controller.ts
@@ -1,7 +1,29 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, Put, Delete, Param, Get, Req } from "@nestjs/common";
+import { type Request } from 'express'
 import { type LoginBody } from "./user.types";
 
 
 @Controller('/user')
 export class UserController { 
+
+  @Get('/')
+  getAllUser() {}
+
+  @Get('/:id')
+  getUserById(@Param('id') id: string) {}
+
+  @Post('/login')
+  login(@Body() body: LoginBody) {}
+
+  @Post('/registration')
+  registration(@Body() body: any) {}
+
+  @Get('/refresh')
+  refreshToken(@Req() req: Request) {}
+
+  @Put('/update/:id')
+  updateUser(@Param('id') id: string, @Body() body: any) {}
+
+  @Delete('/disabled/:id')
+  disabledUser(@Param('id') id: string) {}
 }
